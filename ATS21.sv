c8c05ec80d990d38ab4a77211d2562a9e2623e66
Zack Fravel <zfravel@auto.ece.pdx.edu>
2021-04-20 14:56:43 -0700

    fixed error

--- ATS21.sv
+++ ATS21.sv
@@ -74,7 +74,7 @@ module ATS21 (
   input logic  [15:0] ctrlB,
   output logic        ready,
   output logic [ 1:0] stat,
-  output logic [23:0] data,
+  output logic [23:0] data
 );
 
 ////////////////////////////////////////////////////////////////////
