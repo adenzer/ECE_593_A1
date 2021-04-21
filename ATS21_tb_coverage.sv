3fdc7e89adfa7d456d5f475af597730f5a1cc6f1
Zack Fravel <zfravel@auto.ece.pdx.edu>
2021-04-21 11:02:16 -0700

    fixed error with data coverage

--- ATS21_tb_coverage.sv
+++ ATS21_tb_coverage.sv
@@ -46,7 +46,7 @@ always begin
 end
 
 always_ff @(posedge clk) begin : add_alarms
-	all_alarms <= data[0] + data + data[2] + data[3] + data[4] + data[5] + data[6] + data[7] + data[8] + data[9] + data[10]
+	all_alarms <= data[0] + data[1] + data[2] + data[3] + data[4] + data[5] + data[6] + data[7] + data[8] + data[9] + data[10]
 				+ data[11] + data[12] + data[13] + data[14] + data[15] + data[16] + data[17] + data[18] + data[19] + data[20]
 				+ data[21] + data[22] + data[23];
 end
